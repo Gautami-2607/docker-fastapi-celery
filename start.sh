60ef3e37376bff460bb27f61c14414f2c2c46286
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2024-02-07 10:48:47 +0000

    added start.sh for postgres db

--- start.sh
+++ start.sh
@@ -4,3 +4,13 @@ docker compose up
 # To selectively build container
 # docker compose build <service(s)-name> 
 # docker compose up <service(s)-name> 
+# Postgres loding data
+docker volume ls  //this gives the exact name of volumes availaible
+#now inspect it
+docker volume inspect postgres_postgres_data     
+sudo -i
+cd /workspace/.docker-root/volumes/postgres_postgres_data/_data
+# check weather the data is reflected in the container
+docker exec -it <container name> bash
+cd /var/lib/postgresql/data
+ls -alh
